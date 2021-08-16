1a77c5a232bbb8f94b14abf2d2e389e3f34263a9
Silvan Mosberger <contact@infinisil.com>
2021-08-16 23:04:47 +0200

    A new list option: Declaring generate.requestParams
    Let's introduce another option, generate.requestParams. For its type,
    we'll use `listOf <nestedType>`, which is a generic list type where each
    element has to match the given nested type. In our case we want `str` to
    be the nested type, which is a generic string type.
    
    Note that the difference between `str` and `lines` is in their merging
    behavior:
    - For `lines`, multiple definitions get merged by concatenation with
      newlines
    - For `str`, multiple definitions are not allowed. Which in this case is
      mostly irrelevant however, since we can't really define a list element
      multiple times.

--- default.nix
+++ default.nix
@@ -4,12 +4,21 @@
     generate.script = lib.mkOption {
       type = lib.types.lines;
     };
+
+    generate.requestParams = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+    };
   };
 
   config = {
     generate.script = ''
       map size=640x640 scale=2 | icat
     '';
+
+    generate.requestParams = [
+      "size=640x640"
+      "scale=2"
+    ];
   };
   
 }
