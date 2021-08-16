0ff0c8f08e20b5bc369148babb0785077aabf52d
Silvan Mosberger <contact@infinisil.com>
2021-08-16 23:17:57 +0200

    Conditional definitions: Introducing map.zoom option
    We now introduce the new option `map.zoom` in order to control the zoom
    level of the map. We'll use a new type for it, `nullOr <type>`, which
    accepts the value `null`, but also values of its argument type. We're
    using `null` here to mean an inferred zoom level.
    
    For this option, we'll set a default value which should be used if it's
    not defined otherwise, which we can do using `mkOption`'s `default`
    argument.
    
    Now we want to use this option to define another element in
    `generate.requestParams`, but we only want to add this element if its
    value is non-null. We can do this using the `mkIf <condition>
    <definition>` function, which only adds a definition if the condition
    holds.

--- default.nix
+++ default.nix
@@ -8,6 +8,13 @@
     generate.requestParams = lib.mkOption {
       type = lib.types.listOf lib.types.str;
     };
+
+    map = {
+      zoom = lib.mkOption {
+        type = lib.types.nullOr lib.types.int;
+        default = 2;
+      };
+    };
   };
 
   config = {
@@ -20,6 +27,8 @@
     generate.requestParams = [
       "size=640x640"
       "scale=2"
+      (lib.mkIf (config.map.zoom != null)
+        "zoom=${toString config.map.zoom}")
     ];
   };
   
