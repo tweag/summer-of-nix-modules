6df0f413b9d54b42731e79522f9ba65eb19df52e
Silvan Mosberger <contact@infinisil.com>
2021-08-17 20:08:28 +0200

    Submodule types: Declaring map.markers option
    One of the most useful types of the module system is the `submodule`
    type. This type allows you to define a nested module system evaluation,
    with its own options. Every value of such a type is then interpreted
    (by default) as a `config` assignment of the nested module evaluation.
    
    In this case we're defining a `map.markers` option, whose type is a list
    of submodules with a nested `location` type, allowing us to define a
    list of markers on the map, where each assignment is type checked
    according to the submodule.

--- marker.nix
+++ marker.nix
@@ -1,3 +1,20 @@
-{ lib, config, ... }: {
+{ lib, config, ... }:
+let
+
+  markerType = lib.types.submodule {
+    options = {
+      location = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+      };
+    };
+  };
+in {
+
+  options = {
+    map.markers = lib.mkOption {
+      type = lib.types.listOf markerType;
+    };
+  };
 
 }
